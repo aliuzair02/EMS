22ac3f66ed5955612c9da3d06c4a5f8411910ac6
aliuzair02 <uzairanderson08@gmail.com>
2025-11-30 23:24:53 +0800

    Add files via upload

---
+++ ems-v1/ems/src/main/java/org/example/controllers/TestController.java
@@ -0,0 +1,20 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test1")
+    public String test1(){
+        return "<h1>TEST1</h1>";
+    }
+
+    @GetMapping("/test2/{x}")
+    public String test2(@PathVariable("x") String x){
+        return x;
+    }
+
+}
